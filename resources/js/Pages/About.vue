1133087b2a4b0583c49cc8a0f6a2bb50772f2e78
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-06-30 15:52:09 +0200

    add Home - About Pages

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,14 +1,81 @@
 <template>
-  <h1>About Me</h1>
+    <div class="page">
+
+        <div class="p-5 bg-dark text-white">
+            <div class="container">
+                <div class="top_jumbo d-flex align-items-center gap-3">
+                    <img src="https://www.akibagamers.it/wp-content/uploads/2021/06/jujutsu-kaisen-cover.jpg" alt="Immagine">
+                    <div class="to_jumbo_text">
+                        <h1 class="display-3">About me</h1>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, architecto!</p>
+                    </div>
+                </div>              
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <router-link class="btn btn-primary btn-lg" :to="{ name: 'posts' }" role="button">
+                        Read my posts
+                    </router-link>
+                </p>
+            </div>
+        </div>
+
+        <div class="container py-4">
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eos necessitatibus ex nobis eveniet
+                placeat eius magni neque impedit accusantium ullam aperiam ratione id qui adipisci, expedita autem,
+                minus molestias hic iusto, accusamus in praesentium aliquid sapiente. Rem non, velit repellat id harum
+                laboriosam nam labore consequuntur, pariatur sed porro alias vero qui, placeat illum nihil corporis quis
+                a. Sapiente neque quas rem nostrum? Nesciunt debitis necessitatibus quos provident cum at rerum quis
+                culpa! Facere officiis ab harum in? Tempora quidem harum quisquam, dolorum, pariatur sed deserunt
+                accusantium ut, velit deleniti soluta? Ratione esse accusantium, maxime odit assumenda similique
+                praesentium totam necessitatibus laudantium aliquam pariatur commodi, enim consequuntur quia eius optio
+                ea illum vero quibusdam possimus numquam voluptatem. Blanditiis nemo repellendus ratione eaque, ab fuga
+                id ducimus incidunt facilis veniam, libero odio quaerat perferendis earum error deleniti dolor alias
+                dolorem officiis placeat beatae voluptatum iste! Exercitationem sed eum quod consequuntur aliquid dolor
+                perspiciatis aut libero id. Deserunt, optio est iure id quo expedita repellendus sed, aperiam suscipit
+                facere ut harum rerum nisi autem et, cum vero! Itaque ex corporis possimus dicta saepe delectus
+                reiciendis voluptate minima eius facilis excepturi, quisquam repellat inventore animi corrupti
+                voluptatem laudantium? Suscipit autem dignissimos iusto sed labore voluptates, error quasi rerum
+                voluptas fugit atque nemo non corporis sapiente porro laboriosam dolorem ea vero reiciendis iste
+                recusandae beatae. Facilis facere placeat voluptas totam laudantium voluptates accusantium veritatis.
+                Commodi pariatur ad, iusto velit incidunt unde at consectetur nihil assumenda perspiciatis, quisquam
+                ipsam sint eaque repellat! Id nisi quo inventore corrupti ab impedit animi rerum esse facere dignissimos
+                quia, unde quod, explicabo fugiat libero pariatur ipsa necessitatibus quos, natus repellendus sit? Amet
+                nemo illo accusamus distinctio fuga hic ducimus sapiente delectus fugiat, veritatis perspiciatis ex ipsa
+                voluptatum, dignissimos minima eius. Nesciunt adipisci soluta illum tenetur dolorum corporis amet
+                suscipit, ipsum consectetur aliquid. Alias assumenda voluptate sequi doloribus quis rem nesciunt atque
+                ipsa totam, iusto ab nihil libero rerum. Officia voluptate suscipit nam explicabo natus accusantium
+                porro itaque delectus dolorum incidunt aperiam quia blanditiis quisquam assumenda odit quod, vero minus
+                qui aliquid impedit. Magni, nesciunt illum voluptas magnam aperiam quis quae! Unde sit, dignissimos rem
+                fuga excepturi, autem quisquam deleniti ad corporis repellendus voluptates accusantium, aliquid magnam
+                esse. Unde deserunt veritatis adipisci at nobis cumque, quam, magnam, non repudiandae eum voluptatem
+                mollitia omnis labore voluptates. Beatae, modi aliquid? Eligendi amet voluptatem, modi suscipit, aut
+                voluptate aliquid assumenda accusantium doloribus quasi ipsam veritatis obcaecati cum animi nihil iste
+                repellat fugit eum tempore sint. Aperiam, ipsum aliquid ratione amet error cupiditate ducimus
+                repellendus laborum! Quisquam iste tempore magni vel ad itaque facere porro eum, debitis quod pariatur
+                ex perspiciatis qui quidem illo expedita eaque dicta voluptatem minus non officiis aperiam iure sequi
+                ea! Alias hic, similique recusandae iste numquam saepe perferendis maiores eaque repellendus, eveniet
+                possimus commodi. Nam nemo, tenetur ullam perspiciatis temporibus ipsam ducimus aut hic exercitationem
+                animi impedit ea voluptates minima in error ex eum? Laboriosam ipsam debitis doloremque id aut unde
+                nobis, at, dicta reiciendis velit eos culpa?
+            </p>
+        </div>
+
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'About' 
+    name: 'About'
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+}
 </style>
 
